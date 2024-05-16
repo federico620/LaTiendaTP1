d2d7ca013f7ad492a9083112e34f9dd65989f83b
federico620 <trassanifederico61@gmail.com>
2024-05-16 14:49:52 -0300

    Cambio configuracion para LaTienda.mdf en carpeta de proyecto App_Data

--- LaTienda/LaTiendaModeloEDM.Designer.cs
+++ LaTienda/LaTiendaModeloEDM.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Trassani\source\repos\LaTienda\LaTienda\LaTiendaModeloEDM.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'E:\Proyectos\LaTiendaTP1\LaTienda\LaTiendaModeloEDM.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
